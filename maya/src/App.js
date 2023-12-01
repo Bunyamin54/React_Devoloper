17d3b512f5660b8b840cfbaa9eb3f96843a5af84
Bunyamin54 <bunyaming@uia.no>
2023-12-01 23:47:12 +0100

    JSX HMTL-JAVASCRIPT

--- maya/src/App.js
+++ maya/src/App.js
@@ -4,9 +4,12 @@
 function App() {
   return (
     <div className="App">
-     <h1> Hello World React </h1>
+     <h1> Hello World React </h1>  
     </div>
   );
 }
+
+
+//!  return kismi jsx java script ile -html birlestigi format
 
 export default App;
