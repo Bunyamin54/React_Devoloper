31ff74bcd7cfe8be35a733cbaebc71422dc43366
Bunyamin Genc <bunyamin.genc@tingtun.no>
2023-12-10 00:11:07 +0100

    props added

--- rc02_props/src/App.js
+++ rc02_props/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div>
    <Person 
-   name = "Pappy" 
+   name = "Nicky" 
    img= "https://cdn.pixabay.com/photo/2016/09/05/21/37/cat-1647775_640.jpg"
    color="krem" 
    />
