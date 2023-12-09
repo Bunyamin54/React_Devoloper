80a6c8f85b267f922118b60df23aff7b2a83094c
Bunyamin Genc <bunyamin.genc@tingtun.no>
2023-12-10 00:24:11 +0100

    destucturing methods

--- rc02_props/src/components/Name.jsx
+++ rc02_props/src/components/Name.jsx
@@ -1,11 +1,12 @@
 
 
-const Name = (props) => {
+const Name = ({name}) => {
 
     
   return (
     <div>
- <p>Hello {props.name}</p>
+ <p>Hello {name}</p>
+ {/* <p>Hello {props.name}</p> */}  //! desturucturing methods
     </div>
   )
 }
