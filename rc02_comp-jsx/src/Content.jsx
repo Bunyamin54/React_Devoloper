d2c30a0b6dc3c939b459126bdbdd597cf7760724
Bunyamin Genc <bunyamin.genc@tingtun.no>
2023-12-09 21:59:21 +0100

    inline style

--- rc02_comp-jsx/src/Content.jsx
+++ rc02_comp-jsx/src/Content.jsx
@@ -3,7 +3,7 @@ const Content = () => {
   return (
     <main>
       
-  <h2>React JS</h2>
+  <h2 style={{color: "white", backgroundColor: "red"}}>React JS</h2>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, consectetur, ad numquam porro ratione adipisci assumenda, voluptas dicta ipsa quod nemo soluta similique tempore incidunt magnam rerum non ex accusamus.</p>
 
 
