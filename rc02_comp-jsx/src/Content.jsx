dee8e66ea030af158b932c589d320e8bbfcd1bb2
Bunyamin Genc <bunyamin.genc@tingtun.no>
2023-12-09 23:31:21 +0100

    src eller public img

--- rc02_comp-jsx/src/Content.jsx
+++ rc02_comp-jsx/src/Content.jsx
@@ -1,4 +1,7 @@
 
+ import mandalina from  "./img/mandalina.webp"
+
+
 
 const Content = () => {
 
@@ -28,6 +31,7 @@ const Content = () => {
   <img style={imgStyle} src="https://cdn.pixabay.com/photo/2023/11/26/14/43/girl-8413748_640.png" alt="" />
   <img style={imgStyle} src="https://cdn.pixabay.com/photo/2023/12/04/18/09/icebergs-8430043_640.jpg" alt="" />
   <img src="./assets/island-bird.webp" alt="" />
+   <img src={mandalina} alt="" />
   <p style={parStyle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, consectetur, ad numquam porro ratione adipisci assumenda, voluptas dicta ipsa quod nemo soluta similique tempore incidunt magnam rerum non ex accusamus.</p>
   
   <br />
