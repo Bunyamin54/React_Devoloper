eb7a7c111f198fe83c900b5394fa1b238b6467f9
Bunyamin Genc <bunyamin.genc@tingtun.no>
2023-12-09 22:23:30 +0100

    className style

--- rc02_comp-jsx/src/Content.jsx
+++ rc02_comp-jsx/src/Content.jsx
@@ -28,7 +28,9 @@ const Content = () => {
   <img style={imgStyle} src="https://cdn.pixabay.com/photo/2023/12/04/18/09/icebergs-8430043_640.jpg" alt="" />
   <p style={parStyle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, consectetur, ad numquam porro ratione adipisci assumenda, voluptas dicta ipsa quod nemo soluta similique tempore incidunt magnam rerum non ex accusamus.</p>
 
+  <br />
 
+  <h3>Bu bir h3 elementdir.</h3>
     </main>
   )
 }
