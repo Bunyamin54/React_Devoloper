63fb83e3030d9e037d0bebceb89a211d3a9103bb
Bunyamin Genc <bunyamin.genc@tingtun.no>
2023-12-10 00:13:56 +0100

    props

--- rc02_props/src/App.js
+++ rc02_props/src/App.js
@@ -11,6 +11,18 @@ function App() {
    img= "https://cdn.pixabay.com/photo/2016/09/05/21/37/cat-1647775_640.jpg"
    color="krem" 
    />
+
+<Person 
+   name = "Garfyld" 
+   img= "https://cdn.pixabay.com/photo/2015/11/16/14/43/cat-1045782_640.jpg"
+   color="Gray" 
+   />
+
+<Person 
+   name = "Minnos" 
+   img= "https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_640.jpg"
+   color="'Orange" 
+   />
 </div>
   );
 }
