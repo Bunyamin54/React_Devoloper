e6bb73392c9549b1a6a783758a898b18ad07c357
Bunyamin Genc <bunyamin.genc@tingtun.no>
2023-12-09 22:12:31 +0100

    innline style

--- rc02_comp-jsx/src/Content.jsx
+++ rc02_comp-jsx/src/Content.jsx
@@ -8,13 +8,24 @@ const Content = () => {
       color:"blue",
 
      }
+
+
+       const imgStyle = {
+
+        borderRadius:"20px",
+        width:"400px",
+        cursor:"pointer",
+        display: "block",
+        margin: "auto",
+        padding:"20px"
+       }
   return (
     <main>
       
   <h2 style={{color: "white", backgroundColor: "red"}}>React JS</h2>
-  <img src="https://cdn.pixabay.com/photo/2023/11/30/07/51/bridge-8420945_1280.jpg" alt="" />
-  <img src="https://cdn.pixabay.com/photo/2023/11/26/14/43/girl-8413748_640.png" alt="" />
-  <img src="https://cdn.pixabay.com/photo/2023/12/04/18/09/icebergs-8430043_640.jpg" alt="" />
+  <img style={imgStyle} src="https://cdn.pixabay.com/photo/2023/11/30/07/51/bridge-8420945_1280.jpg" alt="" />
+  <img style={imgStyle} src="https://cdn.pixabay.com/photo/2023/11/26/14/43/girl-8413748_640.png" alt="" />
+  <img style={imgStyle} src="https://cdn.pixabay.com/photo/2023/12/04/18/09/icebergs-8430043_640.jpg" alt="" />
   <p style={parStyle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, consectetur, ad numquam porro ratione adipisci assumenda, voluptas dicta ipsa quod nemo soluta similique tempore incidunt magnam rerum non ex accusamus.</p>
 
 
