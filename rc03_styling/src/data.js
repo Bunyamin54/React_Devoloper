a32c5f434527b77fb4eaf0edc9598490555ac1e5
Bunyamin Genc <bunyamin.genc@tingtun.no>
2023-12-11 15:06:19 +0100

    module css

--- rc03_styling/src/data.js
+++ rc03_styling/src/data.js
@@ -4,13 +4,13 @@ const data = [
     id: 1,
     fagbøker: "Bedriftsøkonomi for IT-studenter",
     img: "https://media.bluestonepim.com/1d982375-6379-47aa-94dd-ca50b2d04f46/b9dc9475-d418-437f-9085-ca8a63bbd248/2khNClLEpw5OjltgtpInitjZW/hVABUhXfHO5Tn22JiJqFBfrB8.jpg?w=800",
-    btnName: " BE-107",
+    btnName: "BE-107",
   },
   {
     id: 2,
     fagbøker: "Digitaliseringens betydning for fremtidens samfunn",
     img: "https://www.akademika.no/sites/default/files/styles/product_large/public/product_images/978/8/2/4/5/0/3/9788245034844.jpg?itok=Vwoe9tcO",
-    btnName: " IS-100",
+    btnName: "IS-100",
   },
   {
     id: 3,
@@ -22,25 +22,25 @@ const data = [
     id: 4,
     fagbøker: "Datakommunikasjon og operativsystem",
     img: "https://media.bluestonepim.com/1d982375-6379-47aa-94dd-ca50b2d04f46/d9c1ee49-58fb-41c9-b31f-568a3d5a4c83/3UEtTKYOM4mdNupPCgnyzas4Q/drjcb56cnU1eA909oDMrOgd9W.jpg?w=800",
-    btnName: " IS-105",
+    btnName: "IS-105",
   },
   {
     id: 5,
     fagbøker: "Objektorientert programmering",
     img: "https://media.bluestonepim.com/1d982375-6379-47aa-94dd-ca50b2d04f46/bf89a6dd-ca4a-4b4b-904a-d39e4d38707b/KtsHAp0rkL20UTHTxRPTj670J/1xdWzQzEcXWgt2ihbHA99bNXD.jpeg?w=800",
-    btnName: " IS-110",
+    btnName: "IS-110",
   },
   {
     id: 7,
     fagbøker: "Introduksjon til samskaping i informasjonssystemer",
     img: "https://media.bluestonepim.com/1d982375-6379-47aa-94dd-ca50b2d04f46/64dab3fd-9ec0-4748-be83-0cff788cd962/cTbJmJ1hqDeKclFDCysBsBf0t/EbKHVE8Afi3AgPxLeoFHS9spE.jpg?w=800",
-    btnName: " IS-114",
+    btnName: "IS-114",
   },
   {
     id: 7,  
     fagbøker: "Organisasjonsteori for IT-studenter",
     img: "https://www.akademika.no/sites/default/files/styles/product_large/public/product_images/978/0/2/7/3/7/6/9780273765608.jpg?itok=dzk58Aoq",
-    btnName: " ORG-110",
+    btnName: "ORG-110",
   },
 ]
 export default data
