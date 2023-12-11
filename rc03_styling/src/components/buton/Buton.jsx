d5a117ca16e821374c8b1cafd68480f2c9ff856e
Bunyamin Genc <bunyamin.genc@tingtun.no>
2023-12-11 16:20:02 +0100

    card style

--- rc03_styling/src/components/buton/Buton.jsx
+++ rc03_styling/src/components/buton/Buton.jsx
@@ -3,8 +3,10 @@ import ButonStyle from "./Buton.module.css"
 
 const buton = ({btn}) => {
   return (
-    <div className="btn-title"> 
-     <button className="btn-white"> {btn} </button>
+    // <div className="btn-title"> 
+    <div className={ButonStyle.btn-title}> 
+     {/* <button className="btn-white"> {btn} </button> */}
+     <button className={["btn-white"]}> {btn} </button>
 
     </div>
  
--- rc03_styling/src/components/card/Card.jsx
+++ rc03_styling/src/components/card/Card.jsx
@@ -8,8 +8,8 @@ const Card = ({ fagbøker, btn, img }) => {
 
   return (
     <div>
-      <h1 className="img-title">{fagbøker}</h1>
-      <img className="images" src={img}  alt="img"/>
+      <h1 className={CardStyle["img-title"]}>{fagbøker}</h1>
+      <img className={CardStyle["images"]} src={img}  alt="img"/>
       <Buton btn={btn}  />
     </div>
   );
