1aad3b46bf485b5f01b26073cbd94ea66d0e166a
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-01-05 23:37:38 +0100

    navbar styl added

--- rc04_horoscope-app-react/src/components/navbar/Navbar.jsx
+++ rc04_horoscope-app-react/src/components/navbar/Navbar.jsx
@@ -1,4 +1,6 @@
 import logo from "../../helpers/logo.png"
+import "./Navbar.scss"
+
 
 const Navbar = () => {
     return (
@@ -6,6 +8,20 @@ const Navbar = () => {
     <div className="logo">
    <img src= {logo} alt="horoscope" />
     </div>
+   <div  className="links">
+    
+    <a href="#horoscope">Horoscope</a>
+    <a href="#daily"> Daily</a>
+    <a href="#tarot">Tarot</a>
+    <a href="#article">Article</a>
+    <a href="#contact">Contact</a>
+    
+    
+     </div>
+
+
+
+
   </div>
 
     )
