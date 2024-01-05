b9c5fe57b40315d6aee3463fc772302b483b9266
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-01-05 23:22:37 +0100

    export default

--- rc04_horoscope-app-react/src/App.js
+++ rc04_horoscope-app-react/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.scss';
 import Header from './components/header/Header';
+import Navbar from './components/navbar/Navbar';
 
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Navbar/>
     </div>
   );
 }
