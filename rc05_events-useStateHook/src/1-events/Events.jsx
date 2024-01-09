4dd8e9f750f4ecb02c817d960fe3730088a6b6f7
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-01-09 21:16:57 +0100

    state dom manipulasyonulari

--- rc05_events-useStateHook/src/1-events/Events.jsx
+++ rc05_events-useStateHook/src/1-events/Events.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 
 const Events = () => {
-
+   let message = "Events"
   const handleClick= (e) =>{
     alert("Hei")
     console.log(e)
   }
 
+    const handleChange =() => {
+       message = "State"   
+    }
   return (
     <div>
+        <h1>{message}</h1>
+
+     
       <button onClick={handleClick}>Click</button>
       <button onClick={() => alert("deneme")}>Save</button>
+      <button  onClick={handleChange}>Change</button>
+   
     </div>
   )
 }
