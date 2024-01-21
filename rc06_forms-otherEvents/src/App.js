4fd281597c403a058ad55142cf40b3687ab22b19
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-01-21 22:59:37 +0100

    onMouseLeave events

--- rc06_forms-otherEvents/src/App.js
+++ rc06_forms-otherEvents/src/App.js
@@ -9,8 +9,8 @@ function App () {
     <div>
       {/* <Form /> */}
       {/* <FormObjectt /> */}
-      <KeyboardEvents />
-      {/* <MouseEvent /> */}
+      {/* <KeyboardEvents /> */}
+      <MouseEvent />
     </div>
   );
 }
