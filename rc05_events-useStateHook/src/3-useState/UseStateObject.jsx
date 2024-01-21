b7657c3e9ee020a547d96f1b71e9d375bbde0fc3
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-01-21 20:04:02 +0100

    forms other events

--- rc05_events-useStateHook/src/3-useState/UseStateObject.jsx
+++ rc05_events-useStateHook/src/3-useState/UseStateObject.jsx
@@ -1,20 +1,38 @@
-import React from 'react'
+import { useState } from 'react'
 
 const UseStateObject = () => {
+  const [name, setName] = useState("Ahmet Can")
+  const [age, setAge] = useState(30)
+  const [salary,setSalary] = useState(50000)
+  
+  const [kisi, setKisi] = useState ({
+   
+    name:"Ahmet Kan",
+    age: 30, 
+    salary:50000
 
-
-
+  })
+  console.log(kisi)
   return (
     <div>
 
-  <h1>USE STATE OBJECT</h1>
-<h2>NAME:</h2>
-<h2>AGE:</h2>
-<h2>SALARY:</h2>
+  {/* <h1>USE STATE OBJECT</h1>
+<h2>NAME: {name}</h2>
+<h2>AGE: {age} </h2>
+<h2>SALARY: {salary} </h2>
 
-<button>change name </button>
-<button>inc age </button>
-<button>inc salary</button>
+<button onClick={() =>setName("Mehmet Kan")}>Change name </button>
+<button onClick={() => setAge(age +1)}>inc age </button>
+<button onClick={() => setSalary (salary + 10000) }>inc salary</button> */}
+
+<h1>USE STATE OBJECT</h1>
+<h2>NAME: {kisi.name}</h2>
+<h2>AGE: {kisi.age} </h2>
+<h2>SALARY: {kisi.salary} </h2>
+
+<button onClick={() =>setKisi({...kisi, name: "Mehmet Kan" })}>Change name </button>
+<button onClick={() => setKisi({...kisi, age:kisi.age +1})}>inc age </button>
+<button onClick={() => setKisi ({...kisi, salary:kisi.salary +10000}) }>inc salary</button>
 
     </div>
   )
