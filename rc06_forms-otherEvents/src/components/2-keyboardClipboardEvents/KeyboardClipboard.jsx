e5c66bf87baec2c4992fe8dbb047a5287fd25b17
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-01-21 22:40:16 +0100

    styling

--- rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++ rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -1,43 +1,51 @@
-import { useState } from "react"
-
+import {useState} from 'react';
 
 const KeyboardClipboard = () => {
+  const [content, setContent] = useState ('');
 
- const [content, setContent] = useState("")
+  const handleKeyDown = e => {
+    if (e.keyCode >= 48 && e.keyCode <= 57) {
+      alert ('Please dont enter a number');
+      e.preventDefault ();
+    }
+  };
 
- const handleKeyDown = (e) => {
-  if( e.keyCode >=48 && e.keyCode <=57) {
-    alert("Please dont enter a number")
-    e.preventDefault()
-  }
- }
-
+  const handlePaste = e => {
+    console.log (e.target);
+    // e.target.style.border = '3px solid red';
+    e.target.className = 'form-control border border-danger border-4';
+  };
   return (
     <div className="container mt-4">
 
+      <h2 className="text-center">Keyboard-Clipboard</h2>
 
-   <h2 className="text-center">Keyboard-Clipboard</h2>
-
-   <input type="text" className="form-control"
-    onChange={ (e) =>{
-      e.target.value = e.target.value.toLocaleUpperCase()
-      setContent (e.target.value)}}
-      onKeyDown={handleKeyDown}
-      value={content}
-      
-      
-      
+      <input
+        type="text"
+        className="form-control"
+        onChange={e => {
+          e.target.value = e.target.value.toLocaleUpperCase ();
+          setContent (e.target.value);
+        }}
+        onKeyDown={handleKeyDown}
+        value={content}
       />
-  
 
- <div className="mt-4">
-<h3>Copied Content</h3>
-<p>{content.toLocaleLowerCase()}</p>
+      <div className="mt-4">
+        <h3>Copied Content</h3>
+        <p>{content.toLocaleLowerCase ()}</p>
+      </div>
 
- </div>
+      <textarea
+        name="form-control"
+        id=""
+        cols="30"
+        rows="10"
+        onPaste={handlePaste}
+      />
 
     </div>
-  )
-}
+  );
+};
 
-export default KeyboardClipboard
+export default KeyboardClipboard;
