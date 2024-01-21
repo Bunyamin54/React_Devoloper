9d2aa4e524c6a177672dbe54fd16dd74210867f6
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-01-21 21:59:18 +0100

    snippet

--- rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++ rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react"
+
+
 const KeyboardClipboard = () => {
+
+ const [content, setContent] = useState(second)
+
 
   return (
     <div className="container mt-4">
@@ -6,8 +12,14 @@ const KeyboardClipboard = () => {
 
    <h2 className="text-center">Keyboard-Clipboard</h2>
 
+   <input type="text" className="form-control" onChange={}/>
 
 
+ <div className="mt-4">
+<h3>Copied Content</h3>
+<p></p>
+
+ </div>
 
     </div>
   )
