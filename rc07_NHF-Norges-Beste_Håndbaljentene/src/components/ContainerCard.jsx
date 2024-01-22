c4a5f5e8bf7e998cf587d5e3a970a39bcae9e5f3
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-01-22 23:00:02 +0100

    import form fra bootstrap react

--- rc07_NHF-Norges-Beste_Håndbaljentene/src/components/ContainerCard.jsx
+++ rc07_NHF-Norges-Beste_Håndbaljentene/src/components/ContainerCard.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+
+
+
 
 const ContainerCard = () => {
-  return <div />;
+  return (
+
+  <>
+      <Form.Control type="text" placeholder="Normal text" />
+  </>
+
+
+  )
 };
 
 export default ContainerCard;
