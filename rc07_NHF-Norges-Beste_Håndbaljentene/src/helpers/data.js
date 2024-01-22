4261e6793bc768566c98e89141575ac0017e2fb0
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-01-22 22:15:58 +0100

    logo og icon og mox data added

---
+++ rc07_NHF-Norges-Beste_Håndbaljentene/src/helpers/data.js
@@ -0,0 +1,150 @@
+export const data = [
+  {
+    name: "Kjersti Grini",
+    img: "https://media.snl.no/media/43154/standard_grini_kjersti.jpg",
+    statistics: [
+      "201 Kamper",
+      "1003 Mål",
+    
+    ],
+  },
+ 
+  {
+    name: "Linn-Kristin Riegelhuth Koren",
+    img: "https://premium.vgc.no/v2/images/d2c61556-143f-4c89-92d4-7b771cfb2ae2?fit=crop&format=auto&h=1425&tight=true&w=1900&s=f2815b3e1ada13df9ea7ba5ab43730d66fc592d9",
+    statistics: [
+      "279 Kamper",
+      "971 Mål",
+    
+    ],
+  },
+ 
+  {
+    name: "Cathrinei 	Roll-Matthiesen",
+    img: "https://gfx.nrk.no/_Mv-eBwSc2nVnQEwa-upzg84rkHETXLOUcrRG9QUoSyQ.jpg",
+    statistics: [
+      "233 Kamper",
+      "918 Mål",
+    
+    ],
+  },
+ 
+  {
+    name: " Susann Goksør Bjerkrheim ",
+    img: "https://akamai.vgc.no/v2/images/778f2da8-2692-454e-a647-4d22b867470c?fit=crop&format=auto&h=1312&w=1698&s=9b475cd6c6d3a5c257482b63688f35bd24897469",
+    statistics: [
+      "296 Kamper",
+      "844 Mål",
+    
+    ],
+  },
+ 
+  {
+    name: "Trine 	Haltvik",
+    img: "https://image.klikk.no/7247822.jpg?imageId=7247822&x=0&y=0&cropw=100&croph=100&width=480&height=399",
+    statistics: [
+      "240 Kamper",
+      "832 Mål",
+    
+    ],
+  },
+ 
+  {
+    name: "Heidi Sundal",
+    img: "https://media.snl.no/media/46290/standard_compressed_sundal_heidi.jpg",
+    statistics: [
+      "268 Kamper",
+      "727 Mål",
+    
+    ],
+  },
+ 
+  {
+    name: "Heidi Løke",
+    img: "https://premium.vgc.no/v2/images/7f300a5a-829b-406f-950b-2c0a739360a0?fit=crop&format=auto&h=1365&w=2048&s=f20f834b4162b9b3d918bc4fcd924fcf69d89ec1",
+    statistics: [
+      "185 Kamper",
+      "692 Mål",
+    
+    ],
+  },
+ 
+  {
+    name: "Gro Hammerseng-Edin",
+    img: "https://premium.vgc.no/v2/images/4cf1b6cd-adae-428b-98df-1cb7fab66cfe?fit=crop&format=auto&h=1202&w=1900&s=ae90dd4d6db4676080bfd2e0a920ed5b5f05adf2",
+    statistics: [
+      "167 Kamper",
+      "631 Mål",
+    
+    ],
+  },
+ 
+  {
+    name: "Camilla Herrem",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Herrem.jpg/800px-Herrem.jpg",
+    statistics: [
+      "223 Kamper",
+      "600 Mål",
+    
+    ],
+  },
+ 
+  {
+    name: "Else-Marthe Sørlie Lybekk",
+    img: "https://premium.vgc.no/v2/images/53e917ab-4f05-45ea-981d-1a2386461f43?fit=crop&format=auto&h=603&w=768&s=cc03ad47b88ab17aa0534e4918ac59cd5ee61d71",
+    statistics: [
+      "215 Kamper",
+      "598 Mål",
+    
+    ],
+  },
+ 
+  {
+    name: "Tonje Sagstuen",
+    img: "https://gfx.nrk.no/11nJJBSEzTuhntLkcOAcbwcYHSrpMfNcpl4lkpIOt5Ow.jpg",
+    statistics: [
+      "217 Kamper",
+      "598 Mål",
+    
+    ],
+  },
+ 
+  {
+    name: "Ingrid Steen",
+    img: "https://media.snl.no/media/140622/standard_compressed_Ingrid_Steen.jpg",
+    statistics: [
+      "160 Kamper",
+      "584 Mål",
+    
+    ],
+  },
+ 
+  {
+    name: "Tonje Larsen",
+    img: "https://akamai.vgc.no/v2/images/ec6d8568-3b9b-48ce-a028-9947d39ebb9e?fit=crop&format=auto&h=294&w=430&s=57c3295b566ac2c7e578e28b541c32bfef8e5f41",
+    statistics: [
+      "264 Kamper",
+      "567 Mål",
+    
+    ],
+  },
+
+  {
+    name: "Nora Mørk",
+    img: "https://premium.vgc.no/v2/images/40fb1b3a-86bf-401b-866d-671677d938d1?fit=crop&format=auto&h=1366&w=2048&s=17c50333df89517c88e86c3f4e8cc20c55fea581",
+    statistics: [
+      "114 Kamper",
+      "560 Mål",
+    
+    ],
+  },
+
+  {
+    name: "Linn Jørum Sulland",
+    img: "https://media.snl.no/media/188857/standard_compressed_Snorroeggen.jpg",
+    statistics: [
+      "183 Kamper",
+      "546 Mål",
+    
+    ],
+  },
