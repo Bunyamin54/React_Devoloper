944d6c3cedc66990195ced75c29004d085f2620e
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-01-23 14:38:52 +0100

    styled

--- rc07_NHF-Norges-Beste_Håndbaljentene/src/components/ContainerCard.jsx
+++ rc07_NHF-Norges-Beste_Håndbaljentene/src/components/ContainerCard.jsx
@@ -28,7 +28,7 @@ const ContainerCard = () => {
       <Form.Control type="search" placeholder="Søk stjerner..." onChange={handleChange} />
 
       <Container className='p-3 rounded-4 card-container my-3 '>
-      <Row xs={2} md={3} lg={4} xl={6} className='justify-content-center'>
+      <Row xs={1} sm={2} md={3} lg={4} className='justify-content-center'>
        
         
         {/* {filteredData.map((player,i) => 
