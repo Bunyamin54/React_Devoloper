fbafefebebfcd3c730a4d413bef7affe85b6d926
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-01-23 14:01:17 +0100

    ternary methods

--- rc07_NHF-Norges-Beste_Håndbaljentene/src/components/PlayerCard.jsx
+++ rc07_NHF-Norges-Beste_Håndbaljentene/src/components/PlayerCard.jsx
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 
 const PlayerCard = ({name, img, statistics}) => {
+  const [showImage, setShowImage] = useState (true);
+
   return (
     <Col>
       <Card>
-        <Card.Img variant="top" src={img} height="350px" />
-        <ul>
-          {statistics.map ((item, i) => (
-            <li className="list-unstyled h5 text-start" key={i}>
-              {' '}🤾‍♀️🤾‍♂️⭐⚽✨ {item}
-            </li>
-          ))}
-
-        </ul>
+        {showImage
+          ? <Card.Img variant="top" src={img} height="350px" />
+          : <ul>
+              {statistics.map ((item, i) => (
+                <li className="list-unstyled h5 text-start" key={i}>
+                  {' '}
+                  🤾‍♀️🤾‍♂️⭐⚽✨ {item}
+                </li>
+              ))}
+            </ul>}
 
         <Card.Footer>
           <Card.Title>{name}</Card.Title>
         </Card.Footer>
       </Card>
-
     </Col>
   );
 };
