4840a7e11f664cb3baed44acf5be9f72dd0a8c2d
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-01-23 11:47:43 +0100

    col row

--- rc07_NHF-Norges-Beste_Håndbaljentene/src/components/ContainerCard.jsx
+++ rc07_NHF-Norges-Beste_Håndbaljentene/src/components/ContainerCard.jsx
@@ -29,9 +29,9 @@ const ContainerCard = () => {
 
       <Container className='p-3 rounded-4 card-container my-3 '>
       <Row xs={2} md={4} lg={6} className='justify-content-center'>
+       
         <Col>1 of 2</Col>
-        <Col>2 of 2</Col>
-        <Col>3 of 3</Col>
+        
       </Row>
      
     </Container>
