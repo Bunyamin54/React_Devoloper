393b2fec1092efe12e0dc3c184dd414000eeaef1
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-01-27 22:55:01 +0100

    bestilling list

--- rc08-appointment-app-starter/src/components/BestillingList.jsx
+++ rc08-appointment-app-starter/src/components/BestillingList.jsx
@@ -1,12 +1,33 @@
 import Container from "react-bootstrap/Container"
+import Col from "react-bootstrap/esm/Col"
+import Row from "react-bootstrap/esm/Row"
 
-const BestillingList = (apps) => {
+const BestillingList = ({apps}) => {
   
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-2" style={{ color: "rgb(166, 18, 189)" }}>
       BestillingList 
       </h3>
+
+   <div>
+
+    <Row>
+
+      <Col>
+      <h4>Tjenestebruker Name</h4>
+      <h4>Tjeneste</h4>  </Col>
+
+      <Col> <h4>Dato:</h4> </Col>
+      <Col> <h4>Icon</h4> </Col>
+
+    </Row>
+   </div>
+
+
+
+
+
     </Container>
   )
 }
