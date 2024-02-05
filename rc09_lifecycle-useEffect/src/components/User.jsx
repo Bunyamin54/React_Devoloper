b14b8412914dd2a7425843756eab55bbed8660eb
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-02-06 00:26:30 +0100

    new Date

--- rc09_lifecycle-useEffect/src/components/User.jsx
+++ rc09_lifecycle-useEffect/src/components/User.jsx
@@ -23,9 +23,9 @@ const User = () => {
         <h4>Hei, My Name is</h4>
         <h1>{userData?.name?.first}  {userData?.name?.last} </h1>
         <h3>{userData?.email}</h3>
-        <h4>{new Date(userData?.dob?.date).toLocaleDateString( "nr-NR")} </h4>
-        <h5> {}  </h5>
-        <h6>address</h6>
+        <h4>{new Date(userData?.dob?.date).toLocaleDateString()} </h4>
+        <h5> {userData?.phone}  </h5>
+        <h6>  {userData?.location?.city} </h6>
 
         <button className="btn btn-success" onClick={getUser}>
           {' '}Get Random User
