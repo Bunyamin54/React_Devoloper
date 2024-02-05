ecff51ebe07d60910b79763962377a107c1cfb76
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-02-06 00:19:52 +0100

    new Date

--- rc09_lifecycle-useEffect/src/components/User.jsx
+++ rc09_lifecycle-useEffect/src/components/User.jsx
@@ -22,9 +22,9 @@ const User = () => {
         <img src={userData?.picture?.large} className='rounded-circle'    alt="" /> 
         <h4>Hei, My Name is</h4>
         <h1>{userData?.name?.first}  {userData?.name?.last} </h1>
-        <h3>Email</h3>
-        <h4>dob</h4>
-        <h5>phone</h5>
+        <h3>{userData?.email}</h3>
+        <h4>{new Date(userData?.dob?.date).toLocaleDateString( "nr-NR")} </h4>
+        <h5> {}  </h5>
         <h6>address</h6>
 
         <button className="btn btn-success" onClick={getUser}>
