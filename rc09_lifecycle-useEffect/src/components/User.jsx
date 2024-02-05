279fb7f8d90f55144afa10c18dd0ee27d71ac8b7
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-02-05 23:49:35 +0100

    fetch data added

--- rc09_lifecycle-useEffect/src/components/User.jsx
+++ rc09_lifecycle-useEffect/src/components/User.jsx
@@ -1,63 +1,35 @@
-import React from 'react'
+import React from 'react';
 
 const User = () => {
+
+
+  let userData =""  //! global degiskene aktariyoruz bu sekilde degisken yazarak
+   // ? Klasik fetch yapisi
+
+  fetch ('https://randomuser.me/api/') // ! istek atiyoruz
+    .then (res => res.json ()) //! veriyi jsonlastiryoruz
+    .then (data => console.log (userData=data))  //! catch blogu atip error de verdirebilriz
+    .catch (err => console.log (err));
+
   return (
     <div>
-     
+
       <img src="" alt="" />
 
-       <h4>Hei , My Name is</h4>
+      <h4>Hei, My Name is</h4>
       <h1>Name</h1>
       <h3>Email</h3>
       <h4>dob</h4>
       <h5>phone</h5>
-
+      <h6>address</h6>
 
       User
 
-
-
-
     </div>
-  )
-}
+  );
+};
 
-export default User
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default User;
 
 // import React, { useEffect, useState } from "react"
 
