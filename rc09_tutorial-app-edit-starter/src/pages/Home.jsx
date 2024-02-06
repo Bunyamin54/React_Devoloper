41d1c1515be11fb4fa1bd26e0f10cd6d9d44a750
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-02-06 20:04:27 +0100

    home added

--- rc09_tutorial-app-edit-starter/src/pages/Home.jsx
+++ rc09_tutorial-app-edit-starter/src/pages/Home.jsx
@@ -1,43 +1,34 @@
-import  { useState,useEffect } from 'react'
-import AddTutorial from '../components/AddTutorial'
-import TutorialList from '../components/TutorialList'
-import axios from 'axios'
+import { useEffect, useState } from "react"
+import AddTutorial from "../components/AddTutorial"
+import TutorialList from "../components/TutorialList"
+import axios from "axios"
 
 const Home = () => {
-  
-  const [tutorials, setTutorials] = useState([]) 
+  const [tutorials, setTutorials] = useState([])
 
-  const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/"
+  const getTutorials = async () => {
+    const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/"
+    try {
+      // const res = await axios(BASE_URL)
+      // setTutorials(res.data)
+      const { data } = await axios(BASE_URL)
+      setTutorials(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
+  console.log(tutorials)
 
+  //? Mount asamasinda api'ye istek atiyoruz
+  useEffect(() => {
+    getTutorials()
+  }, [])
 
-   const getTutorials =async () => {
-
-  try {
-    const{ data }=  await axios("BASE_URL")
-  setTutorials(data)
-  } catch (error) {
-    console.log(error)
-   }
-
-
-   console.log(tutorials)
-
-  //  mount asamasinda apiye istek atiyoruz
-  
-
-   useEffect(() => {
-    
-    getTutorials ()
-   }, [])
-   
   return (
     <>
-    
-    <AddTutorial/>
-    <TutorialList  tutorials={tutorials}/>
-
-      
+      <AddTutorial getTutorials={getTutorials} />
+      <TutorialList tutorials={tutorials} getTutorials={getTutorials} />
     </>
   )
 }
