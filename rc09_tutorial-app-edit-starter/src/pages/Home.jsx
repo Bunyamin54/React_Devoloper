a235d825477e862d04f5ca511969c4bbcc8dde3d
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-02-06 19:55:02 +0100

    TutorialList

--- rc09_tutorial-app-edit-starter/src/pages/Home.jsx
+++ rc09_tutorial-app-edit-starter/src/pages/Home.jsx
@@ -1,24 +1,41 @@
-import React from 'react'
+import  { useState,useEffect } from 'react'
 import AddTutorial from '../components/AddTutorial'
 import TutorialList from '../components/TutorialList'
+import axios from 'axios'
 
 const Home = () => {
+  
+  const [tutorials, setTutorials] = useState([]) 
 
-   const getTutorials = () => {
+  const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/"
 
 
-    
+
+   const getTutorials =async () => {
+
+  try {
+    const{ data }=  await axios("BASE_URL")
+  setTutorials(data)
+  } catch (error) {
+    console.log(error)
    }
 
 
+   console.log(tutorials)
 
+  //  mount asamasinda apiye istek atiyoruz
+  
 
-
+   useEffect(() => {
+    
+    getTutorials ()
+   }, [])
+   
   return (
     <>
     
     <AddTutorial/>
-    <TutorialList/>
+    <TutorialList  tutorials={tutorials}/>
 
       
     </>
