32b197c0d9f3103f5635389c41a10f230e304995
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-02-13 12:06:20 +0100

    NavLink to active class

--- rc10_router-intro-starter/src/components/Nav.jsx
+++ rc10_router-intro-starter/src/components/Nav.jsx
@@ -1,16 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link,NavLink } from "react-router-dom";
 
 const Nav = () => {
+
   return (
     <ul className="bg-warning">
       <li>
-        <Link href="/">Home</Link>
+        <NavLink to="/">Home</NavLink>
       </li>
       <li>
-        <Link Link to ="/instructors">Instructors</Link>
+        <NavLink  to ="/instructors">Instructors</NavLink>
       </li>
       <li>
-        <Link Link to="/contact">Contact</Link>
+        <NavLink to="/contact">Contact</NavLink>
       </li>
     </ul>
   );
