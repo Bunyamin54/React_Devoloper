32195f63847149dca8d979201bff0d9ddf742fc2
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-02-14 12:06:14 +0100

    nestet structure Outlet

--- rc10_router-intro-starter/src/App.js
+++ rc10_router-intro-starter/src/App.js
@@ -6,6 +6,8 @@ import Instructors from "./pages/Instructors";
 import InstructorDetail from "./pages/InstructorDetail";
 import NotFound from "./pages/NotFound";
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import About from "./components/About";
+import Courses from "./components/Courses";
 
 function App() {
   return (
@@ -13,7 +15,16 @@ function App() {
 
      <Nav />
     <Routes>  
-       <Route path="/" element = {<Home/>} />
+       {/* <Route path="/" element = {<Home/>} /><Route/> */}
+       <Route path="/" element = {<Home/>}>  
+        {/* Nested yapi Route icinde Route ile selv klosing ile */}
+        <Route index element={<About/>}/>
+        <Route  path = "courses" element={<Courses/>}/>
+        
+        
+         </Route>
+
+
        <Route path="/instructors" element = {<Instructors/>} />
        <Route path="/instructors/:id" element ={<InstructorDetail/>}  />
        <Route path="/contact" element = {<Contact/>} />
--- rc10_router-intro-starter/src/pages/Home.jsx
+++ rc10_router-intro-starter/src/pages/Home.jsx
@@ -1,11 +1,16 @@
-import About from "../components/About";
-import Courses from "../components/Courses";
+import { Outlet } from "react-router-dom";
+// import About from "../components/About";
+// import Courses from "../components/Courses";
 
 const Home = () => {
   return (
     <div className="p-3">
-      <About />
-      <Courses />
+      {/* <About />
+      <Courses /> */}
+  {/* Nested Route yaåisinda kullandigimiz child componentleri Outlet componenti ile sergiliyoruz */}
+      
+      
+      <Outlet/>
     </div>
   );
 };
