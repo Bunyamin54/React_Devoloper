6833ffc6073d946906cc2e42694dc5e1d2ecd604
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-02-14 13:01:19 +0100

    useLocation

--- rc11-12_router-example-starter/src/pages/PersonDetail.jsx
+++ rc11-12_router-example-starter/src/pages/PersonDetail.jsx
@@ -2,12 +2,24 @@ import React from 'react'
 import { useLocation } from 'react-router-dom'
 
 const PersonDetail = () => {
-let {state} = useLocation()
-console.log(state)
+let {state:person} = useLocation()
+
   return (
+    <div className="container text-center">
+    <h3>
+      {person?.first_name} {person?.last_name}
+    </h3>
+    <img className="rounded" src={person?.avatar} alt="" />
+    <p>{person?.email}</p>
     <div>
-      PersonDetail
+      <button  className="btn btn-success me-2">
+        Go Home
+      </button>
+      <button  className="btn btn-warning">
+        Go Back
+      </button>
     </div>
+  </div>
   )
 }
 
