43a06bcbcfc0a391b12d2d936cba7b89fbffca26
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-02-14 20:20:47 +0100

    outlet

--- rc11-12_router-example-starter/src/router/AppRouter.jsx
+++ rc11-12_router-example-starter/src/router/AppRouter.jsx
@@ -1,34 +1,39 @@
-import React from 'react'
-import Nav from '../components/Nav'
-import { Route, Routes } from 'react-router-dom'
-import Home from '../pages/Home'
-import Paths from '../pages/Paths'
-import People from '../pages/People'
-import PersonDetail from '../pages/PersonDetail'
-import Contact from '../pages/Contact'
-import NotFound from '../pages/NotFound'
-import Footer from '../components/Footer'
+import React from 'react';
+import Nav from '../components/Nav';
+import {Route, Routes} from 'react-router-dom';
+import Home from '../pages/Home';
+import Paths from '../pages/Paths';
+import People from '../pages/People';
+import PersonDetail from '../pages/PersonDetail';
+import Contact from '../pages/Contact';
+import NotFound from '../pages/NotFound';
+import Footer from '../components/Footer';
+import Aws from '../pages/Aws';
+import FullStack from '../pages/FullStack';
 
 const AppRouter = () => {
   return (
     <div>
 
-        <Nav />
+      <Nav />
 
-        <Routes>
-        <Route path="/" element= {<Home />} /> 
-        <Route path="/paths" element={<Paths/>}/>
-        <Route path="/people" element={<People/>}/>
-        <Route path="/people/:id" element={<PersonDetail/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="*" element={<NotFound/>}/>
-        </Routes>
-        <Footer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/paths" element={<Paths />}>
 
+          <Route path="fullstack" element={<FullStack/>} />
+          <Route path="aws" element={<Aws />} />
 
-      
+        </Route>
+        <Route path="/people" element={<People />} />
+        <Route path="/people/:id" element={<PersonDetail />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+
     </div>
-  )
-}
+  );
+};
 
-export default AppRouter
+export default AppRouter;
