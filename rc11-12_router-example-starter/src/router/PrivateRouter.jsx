ff8382d4cd1c443c3009ded429376fd20dae93f1
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-02-19 17:37:21 +0100

    login form bootstrap

--- rc11-12_router-example-starter/src/router/PrivateRouter.jsx
+++ rc11-12_router-example-starter/src/router/PrivateRouter.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Outlet} from 'react-router-dom';
-import Login from '../pages/Login';
+import {Navigate, Outlet} from 'react-router-dom';
+import login from '../pages/Login';
 
 const PrivateRouter = () => {
-  const user = false;
-  return user ? <Outlet /> : <Login />;
+  const user = true;
+  return user ? <Outlet /> : <Navigate to = "/login"/>
 };
 
 export default PrivateRouter;
