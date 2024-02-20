722e0a5966be1a2e42126a9c04eb4e5b7f766ba7
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-02-20 16:46:49 +0100

    useReducer example

--- rc13_reducer/src/reducer.js
+++ rc13_reducer/src/reducer.js
@@ -1,33 +1,33 @@
-
 const initialState = {
+  loading: false,
+  catImage: '',
+  error: '',
+};
 
-   loading : false,
-   catImage: "",
-   error:""
+export const reducer = (initialState, action) => {
+  switch (action.type) {
+    case 'START':
+      return {...initialState, loading: true};
 
-}
+    case 'SUCCES':
+      return {
+        ...initialState,
+        catImage: action.payload,
+        error: '',
+        loading: false,
+      };
+    case 'FAIL':
+      return {
+        ...initialState,
+        catImage: '',
+        error: action.payload,
+        loading: false,
+      };
 
-
-export const reducer = ( initialState, action) =>  {
-
-
-    switch (action.type) {
-        case  "START":
-            
-        return {...initialState, loading:true }
-
-        case "SUCCES" :
-
-        return {...initialState, catImage: action.payload, error:"", loading: false }
-        case "FAIL" :
-
-        return {...initialState, catImage:"",  error: action.payload , loading: false }
-
-        default:
-            break;
-    }
-
-}
+    default:
+      break;
+  }
+};
 
 //? action
 
