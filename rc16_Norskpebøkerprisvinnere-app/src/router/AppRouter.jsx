503615a9a3fe849fe0deb020c397cba3e9aa187d
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-02-26 19:27:46 +0100

    router structures added

--- rc16_Norskpebøkerprisvinnere-app/src/router/AppRouter.jsx
+++ rc16_Norskpebøkerprisvinnere-app/src/router/AppRouter.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
-import Footer from '../components/footer/Footer'
-import {BrowserRouter} from "react-router-dom"
+import React from 'react';
+import Footer from '../components/footer/Footer';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Navbar from '../components/navbar/Navbar';
+import Home from '../pages/home/Home';
+import Login from '../pages/login/Login';
+import Register from '../pages/register/Register';
+import About from '../pages/about/About';
+import Detail from '../pages/detail/Detail';
 const AppRouter = () => {
   return (
-    <BrowserRouter>AppRouter
+    <BrowserRouter>
+      AppRouter
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/" element={<Home />} />
 
-    <Footer/> 
-
+      </Routes>
+      <Footer />
 
     </BrowserRouter>
-   
-  )
-}
+  );
+};
 
-export default AppRouter
+export default AppRouter;
