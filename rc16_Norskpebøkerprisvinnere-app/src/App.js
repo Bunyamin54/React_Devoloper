b165e49b736c2a19c440a2d2b28419b8b1f6bd24
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-02-26 17:57:09 +0100

    theme provider

--- rc16_Norskpebøkerprisvinnere-app/src/App.js
+++ rc16_Norskpebøkerprisvinnere-app/src/App.js
@@ -1,9 +1,13 @@
 import AppRouter from './router/AppRouter';
-
+import {ThemeProvider} from "styled-components"
 function App() {
   return (
     <div className="App">
+      <ThemeProvider>
       <AppRouter/>
+
+      </ThemeProvider>
+   
     </div>
   );
 }
