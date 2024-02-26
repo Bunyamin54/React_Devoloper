912d2f9b937cf65f7a6340d59f8dd5421223422d
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-02-26 19:20:17 +0100

    if else icon light dark

--- rc16_Norskpebøkerprisvinnere-app/src/components/footer/Footer.jsx
+++ rc16_Norskpebøkerprisvinnere-app/src/components/footer/Footer.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import { FooterBtn, FooterContainer } from './Footer.style'
-import { lightTheme } from '../../styles/theme'
-import { lightIcon } from '../../helper/iconData'
+import { darkIcon, lightIcon } from '../../helper/iconData'
+import { useThemeContext } from '../../context/ThemeContext'
 
 const Footer = () => {
+  const {myTheme, setMyTheme} =useThemeContext ();
+
+  const handleClick = () => {
+
+  //   if (myTheme === "light") {
+  //     setMyTheme("dark");
+  //   }else{
+  //  setMyTheme("light");
+
+  //   }
+    setMyTheme(prev => (prev === "light" ? "dark" : "light")) }
+
   return (
    <FooterContainer>
 
-   <FooterBtn>
-
-    {lightIcon}
+   <FooterBtn onClick = {handleClick}>
+  { myTheme === "light" ? lightIcon : darkIcon}
+   
 
    </FooterBtn>
 
