d69c76f1d3b744f3fb13a36b0d179ebb0082d233
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-02-26 19:32:28 +0100

    AuthContextProvider added

--- rc16_Norskpebøkerprisvinnere-app/src/context/AuthContext.jsx
+++ rc16_Norskpebøkerprisvinnere-app/src/context/AuthContext.jsx
@@ -1,1 +1,25 @@
-// Auth Context
+// Auth Context// Theme Context
+
+// Theme Context
+
+import { createContext, useContext, useState } from "react";
+//! Auth context olusturuldu. 
+
+const AuthContext = createContext();
+
+//! Sarmaliyici component provider
+const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState("false");
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+//! consuming custom hook 
+export const useAuthContext = () => {
+    return useContext(AuthContext)
+}
+
+export default AuthContextProvider;
