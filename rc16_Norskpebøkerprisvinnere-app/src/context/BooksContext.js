fc5aed4f854b29d77140c767157ab290064dd0ea
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-02-27 19:30:19 +0100

    env added

--- rc16_Norskpebøkerprisvinnere-app/src/context/BooksContext.js
+++ rc16_Norskpebøkerprisvinnere-app/src/context/BooksContext.js
@@ -1,17 +1,35 @@
 // Books Context// Theme Context
 
-// Theme Context
 
-import { useEffect } from "react";
 import { createContext, useContext, useState } from "react";
+
+import axios from "axios"
 //! Books context olusturuldu. 
 
 const BooksContext = createContext();
 
 //! Sarmaliyici component provider
 const BooksContextProvider = ({ children }) => {
-  
-  
+   const [query ,setQuery] = useState ("")
+   const [selectType, setSelectType] = useState ("all")
+   const [myData, setMyData] = useState ([])
+
+   const APP_KEY= 
+   
+   const getData = async () => {
+   const url = `https://www.googleapis.com/books/v1/volumes?q=${query}&printType=${printType}&key=${APP_KEY}`
+  try {
+    const {data} = await axios(url)
+  } catch (error) {
+    
+  }
+
+
+   }
+
+
+
+   const values= { myData, setMyData}
 
   return (
     <BooksContext.Provider value={{  }}>
