c8bd4b65055801831a2683a05a757b2b2f7513cc
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-02-27 18:15:38 +0100

    header styled added

--- rc16_Norskpebøkerprisvinnere-app/src/components/header/Header.jsx
+++ rc16_Norskpebøkerprisvinnere-app/src/components/header/Header.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
+import React from 'react';
+import {
+  HeaderButton,
+  HeaderContainer,
+  HeaderForm,
+  HeaderTitle,
+  SearchInput,
+  SelectBox,
+} from './Header.style';
 
 const Header = () => {
   return (
     <HeaderContainer>
 
-   <HeaderTitle> Årets Beste Bøker </HeaderTitle>
+      <HeaderTitle> Årets Beste Bøker </HeaderTitle>
 
-   <HeaderForm>
-      <SearchInput/>
-      <SelectBox>
-        <option>Alle</option>
-      </SelectBox>
-      <HeaderButton>Søk....</HeaderButton>
+      <HeaderForm>
+        <SearchInput />
+        <SelectBox>
+          <option>Alle</option>
+        </SelectBox>
+        <HeaderButton>Søk....</HeaderButton>
 
-
-   </HeaderForm>
+      </HeaderForm>
 
     </HeaderContainer>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- rc16_Norskpebøkerprisvinnere-app/src/components/header/Header.style.jsx
+++ rc16_Norskpebøkerprisvinnere-app/src/components/header/Header.style.jsx
@@ -1,1 +1,29 @@
 // Header Style
+import styled from "styled-components"
+import Flex from "../../styles/Flex"
+
+export const HeaderContainer = styled(Flex)`
+flex-direction:column;
+
+`
+
+export const HeaderTitle = styled.h1`
+
+`
+
+export const HeaderForm = styled.form`
+
+`
+
+export const SearchInput= styled.input`
+
+`
+
+export const SelectBox= styled.box`
+
+
+`
+
+export const HeaderButton = styled.button`
+
+`
