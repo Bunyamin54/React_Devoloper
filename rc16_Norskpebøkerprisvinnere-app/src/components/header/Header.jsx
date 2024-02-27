f5bbcf3000b776c0c2eeab1a81f7cb19782e6708
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-02-27 19:46:18 +0100

    search options added

--- rc16_Norskpebøkerprisvinnere-app/src/components/header/Header.jsx
+++ rc16_Norskpebøkerprisvinnere-app/src/components/header/Header.jsx
@@ -7,17 +7,26 @@ import {
   SearchInput,
   SelectBox,
 } from './Header.style';
+import {useBooksContext} from '../../context/BooksContext';
 
 const Header = () => {
+  const printType = ['all', 'books', 'magazines'];
+  const {searchInfo, setSearchInfo, getData} = useBooksContext ();
+  const handleChange = e => {
+    console.log(e.target)
+  }
   return (
     <HeaderContainer>
 
       <HeaderTitle> Årets Beste Bøker </HeaderTitle>
 
       <HeaderForm>
-        <SearchInput type='search'placeholder='søk....' />
-        <SelectBox>
-          <option>Alle</option>
+        <SearchInput type="search" placeholder="søk...." value={searchInfo.query} />
+        <SelectBox value={ searchInfo.selectType} onChange= {handleChange}>
+
+          {printType.map (item => (
+            <option key={item} value={item}>{item} </option>
+          ))}
         </SelectBox>
         <HeaderButton>Søk....</HeaderButton>
 
