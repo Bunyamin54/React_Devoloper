792d9fc46c01a3ae64a2d6059f3597ce9b269ffb
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-02-27 18:32:03 +0100

    header styled input search box and buttons

--- rc16_Norskpebøkerprisvinnere-app/src/components/header/Header.style.jsx
+++ rc16_Norskpebøkerprisvinnere-app/src/components/header/Header.style.jsx
@@ -22,9 +22,9 @@ export const HeaderForm = styled.form`
   flex-wrap: wrap;
   justify-content: center;
   border-radius: 16px;
-  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 4px 30px rgba(195, 74, 74, 0.1);
   backdrop-filter: blur(8.3px);
-  border: 1px solid rgba(15, 63, 42, 0.3);
+  border: 1px solid rgba(82, 212, 156, 0.3);
   margin: 20px;
   background-color: ${({ theme }) => theme.colors.headerFormColor};
   @media (max-width: ${({ theme }) => theme.screens.lg}) {
@@ -36,14 +36,55 @@ export const HeaderForm = styled.form`
 `
 
 export const SearchInput= styled.input`
-
+height: 3rem;
+  width: 55%;
+  border: none;
+  margin-top: 2rem;
+  border-radius: 3px;
+  outline: none;
+  text-indent: 10px;
+  font-size: 2rem;
+  border-top-left-radius: 1rem;
+  border-bottom-left-radius: 1rem;
+  background-color: rgb(255, 255, 255, 0.5);
+  @media (max-width: ${({ theme }) => theme.screens.lg}) {
+    width: 100%;
+    border-radius: 5px;
+  }
 `
 
 export const SelectBox= styled.box`
 
-
+border-radius: 3px;
+  margin-top: 2rem;
+  padding: 5px;
+  height: 3rem;
+  width: 40%;
+  border: none;
+  outline: none;
+  font-size: 2rem;
+  border-top-right-radius: 1rem;
+  background-color: rgb(255, 255, 255, 0.8);
+  border-bottom-right-radius: 1rem;
+  @media (max-width: ${({ theme }) => theme.screens.lg}) {
+    width: 100%;
+    border-radius: 5px;
+  }
 `
 
 export const HeaderButton = styled.button`
-
+ background-color: #e1f1dd;
+  padding: 5px;
+  outline: none;
+  height: 3rem;
+  border: none;
+  margin: 1rem;
+  border-radius: 3px;
+  cursor: pointer;
+  font-size: 2rem;
+  border-top-left-radius: 1rem;
+  border-bottom-right-radius: 1rem;
+  :hover {
+    background-color: ${({ theme }) => theme.colors.navbarBgColor};
+  }
 `
