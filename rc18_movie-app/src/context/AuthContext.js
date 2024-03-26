aa0ab283396effe45b56639b4fb55d87175dde04
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-03-26 18:19:03 +0100

    navigate ('/');

--- rc18_movie-app/src/context/AuthContext.js
+++ rc18_movie-app/src/context/AuthContext.js
@@ -1,39 +1,45 @@
-import React, { createContext} from 'react';
-import {auth} from "../auth/firebase"
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
+import React, {createContext} from 'react';
+import {auth} from '../auth/firebase';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import {useNavigate} from 'react-router-dom';
 
 export const AuthContext = createContext ();
 
-
 const AuthContextProvider = ({children}) => {
-  let navigate = useNavigate()
-  const  createUser = async(email, password) => {
-
+  let navigate = useNavigate ();
+  const createUser = async (email, password) => {
     try {
-     let userCredential = await createUserWithEmailAndPassword(auth, email, password)  
+      let userCredential = await createUserWithEmailAndPassword (
+        auth,
+        email,
+        password
+      );
 
-     console.log(userCredential)
-     navigate("/")
+      console.log (userCredential);
+      navigate ('/');
     } catch (error) {
-
-        console.log(error.message)
+      console.log (error.message);
     }
-  }
+  };
 
-   const signIn = async(email,password) => {
-
-
-
+  const signIn = async (email, password) => {
     try {
-    let userCredential=   await signInWithEmailAndPassword(auth, email, password)  
-    console.log(userCredential)
+      let userCredential = await signInWithEmailAndPassword (
+        auth,
+        email,
+        password
+      );
+      console.log (userCredential);
+      navigate ('/');
     } catch (error) {
-      console.log(error.message)
+      console.log (error.message);
     }
-   }
+  };
 
-  const values = {createUser, signIn}
+  const values = {createUser, signIn};
 
   return (
     <div>
