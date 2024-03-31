56af77570952e0f47f429f5cc0a3598e68621110
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-03-31 14:16:36 +0200

    signUp via google methods

--- rc18_movie-app/src/App.js
+++ rc18_movie-app/src/App.js
@@ -1,19 +1,18 @@
 import React from "react";
-import Register from "./pages/Register";
-import Navbar from "./components/Navbar";
 import AppRouter from "./router/AppRouter";
 import AuthContextProvider from "./context/AuthContext";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
+import MovieContextProvider from "./context/MovieContext";
+
 const App = () => {
   return (
-    <div className="dark:bg-gray-dark-main min-h-screen">
-     <AuthContextProvider>
-     
-     <AppRouter/>
-     <ToastContainer />
-     </AuthContextProvider>
-
- 
+    <div className=" dark:bg-gray-dark-main min-h-screen">
+      <AuthContextProvider>
+        <MovieContextProvider>
+          <AppRouter />
+          <ToastContainer />
+        </MovieContextProvider>
+      </AuthContextProvider>
     </div>
   );
 };
--- rc18_movie-app/src/index.js
+++ rc18_movie-app/src/index.js
@@ -6,4 +6,8 @@ import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render (<BrowserRouter> <App/> </BrowserRouter> );
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
