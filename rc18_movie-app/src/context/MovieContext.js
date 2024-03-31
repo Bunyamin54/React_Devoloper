d1e2097173b6e57377c5909f72d6fe0cd3c08958
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-03-31 14:27:23 +0200

    axios.get(API)

--- rc18_movie-app/src/context/MovieContext.js
+++ rc18_movie-app/src/context/MovieContext.js
@@ -2,7 +2,26 @@ import React, {createContext } from 'react'
 
 
 export const MovieContext =  createContext ()
+
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+
+
+
 const MovieContextProvider = ({children}) => {
+
+
+    const [movies, setMovies] = useState([])
+
+   const getMovies = (API) =>{
+
+   axios.get(API)
+   .then((res )=> console.log(res))
+   .catch((err)) => console.log(err)
+
+   }
+
+
   return (
     <MovieContext.Provider  value={null}> {children}</MovieContext.Provider>
   )
