7165571e4afe253ea0ee823cecce240a06679c8d
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-03-31 18:39:35 +0200

    input and button added

--- rc18_movie-app/src/pages/Main.jsx
+++ rc18_movie-app/src/pages/Main.jsx
@@ -1,18 +1,32 @@
-import React, { useContext } from "react";
-import { MovieContext } from "../context/MovieContext";
+import React, {useContext} from 'react';
+import {MovieContext} from '../context/MovieContext';
+import MovieCard from '../components/MovieCard';
 
 const Main = () => {
+  const {movies, loading} = useContext (MovieContext);
+  console.log (movies);
 
-  const {movies} =useContext(MovieContext)
-  console.log(movies)
+  return (
+    <>
+    <form className='flex justify-center p-2'>
+    <input type="search" className='w-80 h-8 roundended-med p-1 m-2'
+    placeholder='Search a movie...' />
+    <button className='btn-danger-bordered'>Search</button>
+    </form>
 
-  return <div className="flex justify-center flex-wrap">
-  {movies.map(movie => console.log(movie))}
+    <div className="flex justify-center flex-wrap">
+      {loading
+        ? ( <div
+            className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-blue-600 mt-52"
+            role="status"
+          >
+            <span className="visually-hidden">Loading...</span> 
+          </div> )
 
-
-
-
-  </div>;
+        : ( movies.map (movie => <MovieCard key={movie.id} {...movie} />))}
+    </div>  
+    </>
+  );
 };
 
 export default Main;
