03d164c66701fc1c9debf05376fab70ce9fc3b10
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-05-01 20:10:23 +0200

    box added mui

--- rc19_mui-intro-vite/src/components/Typo.jsx
+++ rc19_mui-intro-vite/src/components/Typo.jsx
@@ -1,16 +1,20 @@
 
 // import {Typography} from "@mui/material"
 import Typography from "@mui/material/Typography"  // performas acisindan daha mantikli 
+import Box from '@mui/material/Box';
+
 const Typo = () => {
   return (
-    <div>
+    
+    <Box sx={{padding: "1rem", display:"flex", flexDirection:"column", gap:"1rem" , border:"5px solid tomato" }}>
+    
       <Typography variant="h3" component="h1" color="red" mt={4} > MUI Typo </Typography>
 
       <Typography variant="button"  color="blue"  > MUI Typo </Typography>
       <Typography variant="body1"  color="palegreen"  > Button Text </Typography>
       <Typography variant="body2" sx={{color:"blue", backgroundColor:"pink", mt:"1rem" }}  
-      align="center"  > Sx probu</Typography>
-    </div>
+      align="center"  > SX probu</Typography>
+    </Box>
   )
 }
 
