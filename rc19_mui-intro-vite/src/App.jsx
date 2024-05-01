a3fb548e328c9a4fd4c2f947022b4f4352efc3ce
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-05-01 20:38:12 +0200

    text field inputs

--- rc19_mui-intro-vite/src/App.jsx
+++ rc19_mui-intro-vite/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Typo from './components/Typo'
+import TextFieldComp from './components/TextFieldComp'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
      <Typo/>
+     <TextFieldComp/>
     </>
   )
 }
---
+++ rc19_mui-intro-vite/src/components/TextFieldComp.jsx
@@ -0,0 +1,15 @@
+import { Container, TextField } from '@mui/material'
+
+
+const TextFieldComp = () => {
+  return (
+    <Container  sx={{ mt:4 }} maxWidth="md">
+       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+    </Container>
+  )
+}
+
+export default TextFieldComp
+
