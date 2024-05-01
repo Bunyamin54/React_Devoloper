ab0612c722d49e24b0e0e46b31855c40ccfd0a4b
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-05-01 23:14:07 +0200

    TextField input

--- rc19_mui-intro-vite/src/components/TextFieldComp.jsx
+++ rc19_mui-intro-vite/src/components/TextFieldComp.jsx
@@ -4,7 +4,7 @@ import { Container, TextField } from '@mui/material'
 const TextFieldComp = () => {
   return (
     <Container  sx={{ mt:4 }} maxWidth="md">
-       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+       <TextField id="email" label="Email" variant="outlined" placeholder='Enter your email' required  fullWidth/>
       <TextField id="filled-basic" label="Filled" variant="filled" />
       <TextField id="standard-basic" label="Standard" variant="standard" />
     </Container>
