229c2eddadc9dff267124490e753ada5d3e71cc1
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-05-01 20:17:47 +0200

    muia container sm 600 md 900 lg 1200 xl 1536

--- rc19_mui-intro-vite/src/components/Typo.jsx
+++ rc19_mui-intro-vite/src/components/Typo.jsx
@@ -6,16 +6,21 @@ import Container from '@mui/material/Container';
 
 const Typo = () => {
   return (
-    
-    <Box sx={{padding: "1rem", display:"flex", flexDirection:"column", gap:"1rem" , border:"5px solid tomato" }}>
-    
-      <Typography variant="h3" component="h1" color="red" mt={4} > MUI Typo </Typography>
 
-      <Typography variant="button"  color="blue"  > MUI Typo </Typography>
-      <Typography variant="body1"  color="palegreen"  > Button Text </Typography>
-      <Typography variant="body2" sx={{color:"blue", backgroundColor:"pink", mt:"1rem" }}  
-      align="center"  > SX probu</Typography>
-    </Box>
+    <Container maxWidth="sm">  
+  <Box sx={{padding: "1rem", display:"flex", flexDirection:"column", gap:"1rem" , border:"5px solid tomato" }}>
+    
+    <Typography variant="h3" component="h1" color="red" mt={4} > MUI Typo </Typography>
+
+    <Typography variant="button"  color="blue"  > MUI Typo </Typography>
+    <Typography variant="body1"  color="palegreen"  > Button Text </Typography>
+    <Typography variant="body2" sx={{color:"blue", backgroundColor:"pink", mt:"1rem" }}  
+    align="center"  > SX probu</Typography>
+  </Box>
+
+    </Container>
+    
+    
   )
 }
 
