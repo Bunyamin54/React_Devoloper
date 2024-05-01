49d730813b24577cbbb4bac5e2531445011fb06f
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-05-01 23:19:06 +0200

    TextField

--- rc19_mui-intro-vite/src/components/TextFieldComp.jsx
+++ rc19_mui-intro-vite/src/components/TextFieldComp.jsx
@@ -5,11 +5,11 @@ const TextFieldComp = () => {
   return (
     <Container  sx={{ mt:4 }} maxWidth="md">
        <TextField id="email" label="Email" variant="outlined" placeholder='Enter your email' required  fullWidth/>
-      <TextField id="filled-basic" label="Filled" variant="filled" />
-      <TextField id="standard-basic" label="Standard" variant="standard" />
+      <TextField id="password" label="password" variant="filled" placeholder='Enter your password' required fullWidth  margin='normal'/>
+      <TextField id="name"  label="Name" variant="standard" placeholder='Enter your name' required fullWidth margin='normal' />
     </Container>
   )
-}
+}  
 
 export default TextFieldComp
 
