4a29fbeefb6741c9edea0b6ba8a7b1b0ad2b6778
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-05-01 20:00:02 +0200

    typhography mui

--- rc19_mui-intro-vite/src/components/Typo.jsx
+++ rc19_mui-intro-vite/src/components/Typo.jsx
@@ -4,7 +4,11 @@ import Typography from "@mui/material/Typography"  // performas acisindan daha m
 const Typo = () => {
   return (
     <div>
-      <Typography variant="h3" component="h1" > MUI Typo </Typography>
+      <Typography variant="h3" component="h1" color="red" mt={4} > MUI Typo </Typography>
+
+      <Typography variant="button"  color="blue"  > MUI Typo </Typography>
+      <Typography variant="body1"  color="palegreen"  > Button Text </Typography>
+      <Typography variant="body2" sx={{color:"blue", backgroundColor:"pink", mt:"1rem" }}  > Sx probu</Typography>
     </div>
   )
 }
