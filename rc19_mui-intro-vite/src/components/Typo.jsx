08ad402894b4f0f4470281f6c9daeee8cb71e1f2
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-05-01 20:27:40 +0200

    Stack mui

--- rc19_mui-intro-vite/src/components/Typo.jsx
+++ rc19_mui-intro-vite/src/components/Typo.jsx
@@ -1,28 +1,45 @@
-
 // import {Typography} from "@mui/material"
-import Typography from "@mui/material/Typography"  // performas acisindan daha mantikli 
+import Typography from '@mui/material/Typography'; // performas acisindan daha mantikli
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Stack from '@mui/material/Stack';
 
 const Typo = () => {
   return (
+    <Container maxWidth="md">
+      <Box
+        sx={{
+          padding: '1rem',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '1rem',
+          border: '5px solid tomato',
+        }}
+      >
 
-    <Container maxWidth="md">  
-  <Box sx={{padding: "1rem", display:"flex", flexDirection:"column", gap:"1rem" , border:"5px solid tomato" }}>
-    
-    <Typography variant="h3" component="h1" color="red" mt={4} > MUI Typo </Typography>
+        <Typography variant="h3" component="h1" color="red" mt={4}>
+          {' '}MUI Typo{' '}
+        </Typography>
 
-    <Typography variant="button"  color="blue"  > MUI Typo </Typography>
-    <Typography variant="body1"  color="palegreen"  > Button Text </Typography>
-    <Typography variant="body2" sx={{color:"blue", backgroundColor:"pink", mt:"1rem" }}  
-    align="center"  > SX probu</Typography>
-  </Box>
+        <Typography variant="button" color="blue"> MUI Typo </Typography>
+        <Typography variant="body1" color="palegreen"> Button Text </Typography>
+        <Typography
+          variant="body2"
+          sx={{color: 'blue', backgroundColor: 'pink', mt: '1rem'}}
+          align="center"
+        >
+          {' '}SX probu
+        </Typography>
+      </Box>
 
+      <Stack direction="row" spacing={1} mt={4}justifyContent="center" alignItems="center">
+        <p>Click</p>
+        <p>Click</p>
+        <p>Click</p>
+
+      </Stack>
     </Container>
-    
-    
-  )
-}
+  );
+};
 
-export default Typo
+export default Typo;
