cbfb70d8efd0d0d3550ffb047a8dcdca5efc115e
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-05-09 14:52:40 +0200

    create theme

--- rc19_mui-intro-vite/src/App.jsx
+++ rc19_mui-intro-vite/src/App.jsx
@@ -4,10 +4,12 @@ import TextFieldComp from './components/TextFieldComp'
 import CardComp from './components/CardComp'
 import { AppBar } from '@mui/material'
 import AppbarComp from './components/AppbarComp'
+import { createTheme, ThemeProvider } from '@mui/material'
+
 
 function App() {
 
-
+const theme=createTheme
   return (
     <>
      {/* <Typo/>
