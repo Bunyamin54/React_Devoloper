12f6e47fcd45439abe76c84c9463c97666a455b2
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-05-10 13:41:12 +0200

    combineReducers

--- rc20_redux-intro/src/store/index.jsx
+++ rc20_redux-intro/src/store/index.jsx
@@ -1,7 +1,18 @@
 
-import { legacy_createStore as createStore} from "redux"
+import { legacy_createStore as createStore, combineReducers} from "redux"
 import { counterReducer } from "./counterReduser"
+import { todoReducer } from "./todoReduser"
 
 // ! storu olusturduk
 
-export const store = createStore (counterReducer)
+// export const store = createStore (counterReducer)
+
+const rootReducer = combineReducers({
+
+ counterReducer,
+ todoReducer,
+
+
+})
+
+export const store = createStore(rootReducer)
