3f90b09a1f45bd48801c861168c2ddd60021f2bd
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-05-10 14:28:24 +0200

    todoReducer

--- rc20_redux-intro/src/store/counterReduser.js
+++ rc20_redux-intro/src/store/counterReduser.js
@@ -1,28 +1,34 @@
-//? counter type declaration 
+//? counter type declarations
+//* string'ler hataya meyillidir.Bu hatalari minimize etmek icin string type'lari degiskenlere saklanir ve degisken olarak kullanilabilir.
 export const INC = "INC"
-export const DEC = 'DEC'
-export const CLR = 'CLR'
+export const DEC = "DEC"
+export const CLR = "CLR"
 
-
-const initialState = {
-    count: 0,
+//! action islemlerini basitlestirmek icin fonksiyonlar yazilabilir.
+export const increment = () => {
+  return { type: INC }
 }
-// PURE REDUSER FUNCTION
+export const decrement = () => ({ type: DEC })
+export const clear = () => ({ type: CLR })
+
+//? State'lerin baslangic durumlarinin belirlenmesi
+const initialState = {
+  count: 0,
+}
+
+//? Pure Function (reducer)
 export const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INC: //? count:  state.count + 1
+      return { count: state.count + 1 }
 
+    case DEC:
+      return { count: state.count - 1 }
 
-    switch (action.type) {
-        case INC:
-            return{count: state.count +1}
-          
-            case DEC:
-            
-             return { count: state.count -1}
-            case CLR:
-              return { count:0 }
-           
-        default:
-            return state
-            //! reduser funkction must have a return or the first stiasjon
-    }
+    case CLR:
+      return { count: 0 }
+    default:
+      return state
+    //! Reducer fonksiyonu muhakkak bir state objesi dondurmelidir. Yanlis bir type gelse bile state'in ilk halini dondurmelidir.
+  }
 }
--- rc20_redux-intro/src/store/todoReduser.js
+++ rc20_redux-intro/src/store/todoReduser.js
@@ -1,39 +1,40 @@
 //? todoReducer type declaration
+export const ADD_TODO = "ADD_TODO"
+export const DELETE_TODO = "DELETE_TODO"
+export const TOGGLE_TODO = "TOGGLE_TODO"
+export const CLEAR_TODO = "CLEAR_TODO"
 
-export const  ADD_TODO = 'ADD_TODO'
-export const DELETE_TODO = 'DELETE_TODO'
-export const TOGGLE_TODO = 'TOGGLE_TODO'
-export const CLEAR_TODO = 'CLEAR_TODO'
-
-
-//? action funtion declaration
-
+//? action function declartion
 export const addTodo = (payload) => ({ type: ADD_TODO, payload })
-export const clearTodo= ()=> ({type: CLEAR_TODO})
+export const clearTodo = () => ({ type: CLEAR_TODO })
 
-
-
-
-
-
-
-//?  baslangic durumnlari
+//? baslangic durumlari
 const initialState = {
-
-  todoList: [{ id: new Date(). getTime(), text: "work redux", competed: true}]
-
+  todoList: [{ id: new Date().getTime(), text: "work redux", completed: true }],
 }
 
-export const todoReducer (state = initialState, { type, payload }) => {
+export const todoReducer = (state = initialState, { type, payload }) => {
   switch (type) {
-
-  case ADD_TODO:
-    return { todoList: [...state.todoList,  {id:new Date(). getTime(), text:payload, competed:false}]}
+    case ADD_TODO:
+      return {
+        todoList: [
+          ...state.todoList,
+          { id: new Date().getTime(), text: payload, completed: false },
+        ],
+      }
 
     case CLEAR_TODO:
-    return initialState
+      return initialState
 
-  default:
-    return state
+    //TODO
+    case DELETE_TODO:
+      return initialState
+
+    //TODO
+    case TOGGLE_TODO:
+      return initialState
+
+    default:
+      return state
   }
 }
