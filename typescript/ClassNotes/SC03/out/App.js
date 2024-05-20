6969ebaa6bc3cff9bc73b40a7888546ea6b92dad
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-05-20 17:08:36 +0200

    interface implements

---
+++ typescript/ClassNotes/SC03/out/App.js
@@ -0,0 +1,38 @@
+//? Interface
+const person1 = {
+    name: 'Mark'
+};
+//? Interface
+// const person1 = {
+//     name: 'Mark'
+// };
+// interface Calender {
+//     events: string [];
+//     addEvents(event: string) : void;
+// }
+// class LMSCalender implements Calender {
+//     constructor(public events: string[]) {}
+//     addEvents(events: string) :void {
+//         this.events.push(events);
+//     }
+// }
+// const cohort14= new LMSCalender(['HTML', 'JS'])
+// cohort14.addEvents('React')
+// console.log(cohort14)
+//? Interface 
+// interface Color {
+//     color: { r: number, g: number, b: number}
+// }
+// const red:Color = { color: {r:255, g:0, b:0}}
+// interface Shape {
+//     area: number;
+// }
+// let shape1 : Shape = {area:255}
+// class Square implements Color, Shape {
+//     constructor (public color: {r: number, g: number, b:number} public area:number) {
+//     }
+// }
+// const sq = new Square({r: 255, g:0, b:0}  ,255)
+// const sq = new Square(red.color  ,255)
+// const sq = new Square(red.color  ,shape1.area)
+// console.log(sq)
