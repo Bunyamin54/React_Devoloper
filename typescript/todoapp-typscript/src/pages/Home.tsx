627d561b91f34ce84a6e836e6bd4cfe0eee11063
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-05-30 13:43:23 +0200

    interface added

--- typescript/todoapp-typscript/src/pages/Home.tsx
+++ typescript/todoapp-typscript/src/pages/Home.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
+interface TodoType {
+
+  todo: string;
+  isDone:boolean;
+  id: string | number //? iki veri tpinide kabul edecek 
+}
+
 const Home = () => {
+   const [todos,setTodos] = useState([])
+
+
+
   return (
     <div>
-      
+      <Home/>
     </div>
   )
 }
