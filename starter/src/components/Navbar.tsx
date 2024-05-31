d4cfc29d5424c88610edf4b7e14cfb46cd15b9d2
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-05-31 13:17:20 +0200

    navbar added

--- starter/src/components/Navbar.tsx
+++ starter/src/components/Navbar.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='flex justify-between h-[50px] text-white bg-gray-500 items-center px-5'>
+      <h3 className='font-bold italic'>Claruse store</h3>
+      <div>
+       
+     <Link to ="/" className='mr-2'>Home</Link>
+     <Link to ="/">Favorites</Link>
+
+
+
+
+      </div>
     </div>
   )
 }
