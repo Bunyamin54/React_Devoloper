efe797b0c4dc7d8746fab3a3ea1c20cff69d9834
Bunyamin Genc <bunyamin.genc@tingtun.no>
2024-05-31 13:09:07 +0200

    folder structured

--- starter/src/App.tsx
+++ starter/src/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import FavoritesPage from "./pages/FavoritesPage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="bg-slate-500 min-h-screen text-center">
       <BrowserRouter>
+      <Navbar/>
       <Routes>
 
-     <Route path="/" element={} />
-     <Route path="/favorites" element={} />
+     <Route path="/" element={<Home/>} />
+     <Route path="/favorites" element={<FavoritesPage/>} />
 
 
 
